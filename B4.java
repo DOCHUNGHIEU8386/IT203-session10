81c214edac10d89c39a49b4ee80c0c00867f982f
DOCHUNGHIEU8386 <quangngochieu266@gmail.com>
2026-02-23 13:48:55 +0700

    Update B4.java

--- B4.java
+++ B4.java
@@ -1,45 +1,27 @@
-class Employee {
-    private String employeeId;
-    private String employeeName;
-    private double salary;
+interface RemoteControl {
 
-    // constructor khong tham so
-    public Employee(){
-        this.employeeId = "";
-        this.employeeName = "";
-        this.salary = 0;
-    }
+    void powerOn();
 
-    // constructor co ma va ten
-    public Employee(String employeeId , String employeeName){
-        this.employeeId = employeeId;
-        this.employeeName = employeeName;
-        this.salary = 0;
-    }
-
-    //constructor co day du
-    public Employee(String employeeId , String employeeName , double salary){
-        this.employeeId = employeeId;
-        this.employeeName = employeeName;
-        this.salary = salary;
-    }
-
-    public void displayInfo(){
-        System.out.println("Ma nhan vien :"+employeeId);
-        System.out.println("Ten nhan vien :"+employeeId);
-        System.out.println("Luong :"+salary);
-        System.out.println("--------------------");
+    default void checkBattery() {
+        System.out.println("Pin ổn định");
     }
 }
 
-public class B4 {
-    public static void main(String[] args){
-        Employee employee1 = new Employee();
-        Employee employee2 = new Employee("NV1" , "Ngo quang anh");
-        Employee employee3 = new Employee("NV2" , "Anh Quang" , 50000000);
+public class Main {
 
-        employee1.displayInfo();
-        employee2.displayInfo();
-        employee3.displayInfo();
+    public static void main(String[] args) {
+
+        // Anonymous Class
+        RemoteControl smartLight = new RemoteControl() {
+
+            @Override
+            public void powerOn() {
+                System.out.println("Đèn đã bật");
+            }
+        };
+
+        // Gọi phương thức
+        smartLight.powerOn();
+        smartLight.checkBattery();
     }
 }
